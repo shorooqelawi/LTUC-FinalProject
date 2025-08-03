5b9b4f0ae660e24d4c1cc1d8651c4c36b813fdf1
shorooq elawi <shorooqelawi@gmail.com>
2025-08-03 17:08:40 +0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# LTUC-FinalProject" 
+"# LTUC-FinalProject" 
---
+++ src/main/java/LTUC_FinalProject/LTUC_FinalProject/App.java
@@ -0,0 +1,10 @@
+package LTUC_FinalProject.LTUC_FinalProject;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
