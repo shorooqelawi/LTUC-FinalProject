e732f40059a945accfa619c48a6cd65ac3ebec61
shorooq elawi <shorooqelawi@gmail.com>
2025-08-03 23:10:26 +0300

    Update: وصف التغييرات التي أجريتها

---
+++ src/test/java/pages/SignUpPage.java
@@ -0,0 +1,44 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class SignUpPage {
+    WebDriver driver;
+
+    public SignUpPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    By firstName = By.id("first_name");
+    By lastName = By.id("last_name");
+    By dob = By.id("dob");
+    By street = By.id("street");
+    By postalCode = By.id("postal_code");
+    By city = By.id("city");
+    By state = By.id("state");
+    By country = By.id("country");
+    By phone = By.id("phone");
+    By email = By.id("email");
+    By password = By.id("password");
+    By submitBtn = By.xpath("//button[@type='submit']");
+
+    // هاد ميثود يعبي الفورم
+    public void fillForm(String fName, String lName, String date, String str, String zip, String ct, String st, String cntry, String phn, String em, String pwd) {
+        driver.findElement(firstName).sendKeys(fName);
+        driver.findElement(lastName).sendKeys(lName);
+        driver.findElement(dob).sendKeys(date);
+        driver.findElement(street).sendKeys(str);
+        driver.findElement(postalCode).sendKeys(zip);
+        driver.findElement(city).sendKeys(ct);
+        driver.findElement(state).sendKeys(st);
+        driver.findElement(country).sendKeys(cntry);
+        driver.findElement(phone).sendKeys(phn);
+        driver.findElement(email).sendKeys(em);
+        driver.findElement(password).sendKeys(pwd);
+    }
+
+    public void submit() {
+        driver.findElement(submitBtn).click();
+    }
+}
--- src/test/java/tests/TestCases.java
+++ src/test/java/tests/TestCases.java
@@ -1,5 +1,69 @@
 package tests;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import pages.SignUpPage;
+
 public class TestCases {
 
+    WebDriver driver = new ChromeDriver();
+    String TheURL = "https://practicesoftwaretesting.com/";
+    Connection con;
+
+    @BeforeTest
+    public void MySetUp() throws SQLException {
+        con = DriverManager.getConnection("jdbc:mysql://localhost:3306/classicmodels", "root", "shorooq2002_666");
+        driver.get(TheURL);
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void SignUpPage() throws InterruptedException, SQLException {
+        Thread.sleep(1000);
+        driver.navigate().to("https://practicesoftwaretesting.com/auth/register");
+
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM customers");
+
+        while (rs.next()) {
+            String firstName = rs.getString("contactFirstName");
+            String lastName = rs.getString("contactLastName");
+            String street = rs.getString("addressLine1");
+            String city = rs.getString("city");
+            String state = rs.getString("state") != null ? rs.getString("state") : "DefaultState";
+            String postalCode = rs.getString("postalCode");
+            String country = rs.getString("country") != null ? rs.getString("country") : "Jordan";
+            String phone = rs.getString("phone");
+
+            if (firstName == null || lastName == null || street == null || phone == null) {
+                continue;
+            }
+
+            // إزالة الرموز من رقم الهاتف
+            phone = phone.replaceAll("[^0-9]", "");
+
+            // توليد إيميل بدون نقطة
+            int randomNum = (int)(Math.random() * 10000);
+            String email = lastName.toLowerCase()+ randomNum+"@test.com";
+
+            String password = "UserPass@1233";
+            String dob = "1995-01-01";
+
+            SignUpPage signup = new SignUpPage(driver);
+            signup.fillForm(firstName, lastName, dob, street, postalCode, city, state, country, phone, email, password);
+            signup.submit();
+
+            break;
+        }
+
+    }
 }
+
